e2b642494d004a0c436727d0f20745790fac94a2
ShaheerAliDanish <shaheeralidanish@gmail.com>
2023-12-01 00:07:31 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var multiply=100;
+
+var Maths=prompt("Enter your Maths Marks");
+var English=prompt("Enter your English Marks");
+var Chemistry=prompt("Enter Your Chemistry Marks");
+var PST=prompt("Enter Your P.S.T Marks");
+var Islamiat=prompt("Enter your Islamiat Marks")
+
+
+var Maths_Marks = Maths * multiply / 75;
+var English_Marks = English * multiply / 100;
+var Chemistry_Marks = Chemistry * multiply /60;
+var PST_Marks = PST * multiply /75;
+var Islamiat_Marks = Islamiat * multiply /75;
+
+alert("Math Obtaine Marks :" + Maths +"Out of-" + 75 +"Percentage is:" + Maths_Marks );
+alert("English Obtaine Marks :" + English +"Out of-" + 100 +"Percentage is " + English_Marks);
+alert("Chemistry Obtaine Marks :" + Chemistry +"Out of-" + 60  +"Percentage is " + Chemistry_Marks);
+alert("PST Obtaine Marks :" + PST +"Out of-" + 75 +"Percentage is " + PST_Marks);
+alert("Islamiat Obtaine Marks :" + Islamiat +"Out of-" + 75 +"Percentage is " + Islamiat_Marks);
+
+
+
+
+
+
+
